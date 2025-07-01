c5c6c31846a262b7de6d06bf9258833de76947c8
Tuan Khai <dcme.zx@gmail.com>
2025-07-02 00:35:42 +0700

    add update-profile

--- app/src/main/java/com/example/doctorappoint/data/services/UserApi.kt
+++ app/src/main/java/com/example/doctorappoint/data/services/UserApi.kt
@@ -1,8 +1,13 @@
 package com.example.doctorappoint.data.services
 
 import com.example.doctorappoint.model.LoginResponse
+import com.example.doctorappoint.model.UpdateProfile
+import com.example.doctorappoint.model.UpdateProfileResponse
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface UserApi {
@@ -13,4 +18,15 @@ interface UserApi {
         @Field("phone") phone: String,
         @Field("password") password: String
     ): LoginResponse
+
+    @POST("/logout")
+    suspend fun userLogout(
+        @Header("Authorization") token: String
+    ): Response<Unit>
+
+    @POST("/update-profile")
+    suspend fun updateProfile(
+        @Header("Authorization") token: String,
+       @Body updateProfile: UpdateProfile
+    ):UpdateProfileResponse
 }
